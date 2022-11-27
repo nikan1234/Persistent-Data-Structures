4a57f64e5f43fd495d019086250031c7b55ec3bc
g.bykova <g.bykova@g.nsu.ru>
2022-11-27 16:34:42 +0700

    list undo/redo, iterator++, --, reverse

--- test/Collections/PersistentListTests.cpp
+++ test/Collections/PersistentListTests.cpp
@@ -26,66 +26,66 @@ TEST(PersistentListTests, Order) {
 TEST(PersistentListTests, AddNode) { 
     std::shared_ptr<ListOrder> listOrder = std::make_shared<ListOrder>(); 
     listOrder->add(1);
-    ListNode listNode(1, 10, nullptr, nullptr, CmpByListVersion(listOrder)); 
-    for (int i = 1; i < ListNode::MAX_SIZE_FAT_NODE; ++i) {
-      ASSERT_EQ(listNode.Add(listOrder->add(i), i), true);
+    ListNode<int> listNode(1, 10, nullptr, nullptr, CmpByListVersion(listOrder)); 
+    for (int i = 1; i < ListNode<int>::MAX_SIZE_FAT_NODE; ++i) {
+      ASSERT_EQ(listNode.add(listOrder->add(i), i), true);
     }
-    ASSERT_EQ(listNode.Add(listOrder->add(ListNode::MAX_SIZE_FAT_NODE), 10), false);
+    ASSERT_EQ(listNode.add(listOrder->add(ListNode<int>::MAX_SIZE_FAT_NODE), 10), false);
 }
 
 // проверяем, что в одной ноде корректно ищется значение по версии
 TEST(PersistentListTests, FindNode) {
   std::shared_ptr<ListOrder> listOrder = std::make_shared<ListOrder>();
   listOrder->add(1);
-  ListNode listNode(1, 10, nullptr, nullptr, CmpByListVersion(listOrder));
-  listNode.Add(listOrder->add(1), 11); // 2
-  listNode.Add(listOrder->add(1), 12); // 3
-  listNode.Add(listOrder->add(2), 13); // 4
-  listNode.Add(listOrder->add(1), 14); // 5
-  listNode.Add(listOrder->add(2), 15); // 6
+  ListNode<int> listNode(1, 10, nullptr, nullptr, CmpByListVersion(listOrder));
+  listNode.add(listOrder->add(1), 11); // 2
+  listNode.add(listOrder->add(1), 12); // 3
+  listNode.add(listOrder->add(2), 13); // 4
+  listNode.add(listOrder->add(1), 14); // 5
+  listNode.add(listOrder->add(2), 15); // 6
   listOrder->add(4); // изменение для 7 версии не в этой ноде
   listOrder->add(6); // изменение для 8 версии не в этой ноде
-  ASSERT_EQ(listNode.Find(3), 12);
-  ASSERT_EQ(listNode.Find(4), 13);
-  ASSERT_EQ(listNode.Find(7), 13);
-  ASSERT_EQ(listNode.Find(8), 15);
+  ASSERT_EQ(listNode.find(3), 12);
+  ASSERT_EQ(listNode.find(4), 13);
+  ASSERT_EQ(listNode.find(7), 13);
+  ASSERT_EQ(listNode.find(8), 15);
 
-  ListNode listNode1(3, 10, nullptr, nullptr, CmpByListVersion(listOrder));
-  listNode1.Add(7, 11);
-  ASSERT_EQ(listNode1.Find(4), 10);
+  ListNode<int> listNode1(3, 10, nullptr, nullptr, CmpByListVersion(listOrder));
+  listNode1.add(7, 11);
+  ASSERT_EQ(listNode1.find(4), 10);
 }
 
 TEST(PersistentListTests, FindList) {
-  const PersistentList test({1, 2, 3, 4});
-  ASSERT_EQ(test.Find(0), 1);
-  ASSERT_EQ(test.Find(1), 2);
-  ASSERT_EQ(test.Find(2), 3);
-  ASSERT_EQ(test.Find(3), 4);
+  const PersistentList<int> test({1, 2, 3, 4});
+  ASSERT_EQ(test.find(0), 1);
+  ASSERT_EQ(test.find(1), 2);
+  ASSERT_EQ(test.find(2), 3);
+  ASSERT_EQ(test.find(3), 4);
 }
 
 TEST(PersistentListTests, SetList) {
-  PersistentList v1({1, 2, 3, 4});
-  auto v2 = v1.Set(0, -1);
-  auto v3 = v1.Set(1, -2);
-  auto v4 = v2.Set(2, -3);
+  PersistentList<int> v1({1, 2, 3, 4});
+  auto v2 = v1.set(0, -1);
+  auto v3 = v1.set(1, -2);
+  auto v4 = v2.set(2, -3);
   //                v1 {1, 2, 3, 4}
   //                   /           \
   //         v2 {-1, 2, 3, 4}     v3 {1, -2, 3, 4}
   //                 /
   //    v4 {-1, 2, -3, 4}
-  ASSERT_EQ(v2.Find(0), -1);
-  ASSERT_EQ(v3.Find(0), 1);
-  ASSERT_EQ(v3.Find(1), -2);
-  ASSERT_EQ(v4.Find(2), -3);
-  ASSERT_EQ(v4.Find(0), -1);
+  ASSERT_EQ(v2.find(0), -1);
+  ASSERT_EQ(v3.find(0), 1);
+  ASSERT_EQ(v3.find(1), -2);
+  ASSERT_EQ(v4.find(2), -3);
+  ASSERT_EQ(v4.find(0), -1);
 }
 
 TEST(PersistentListTests, EraseList) {
-  PersistentList v1({1, 2, 3, 4});
-  auto v2 = v1.Erase(1);
-  auto v3 = v1.Erase(2);
-  auto v4 = v2.Erase(2);
-  auto v5 = v4.Erase(0);
+  PersistentList<int> v1({1, 2, 3, 4});
+  auto v2 = v1.erase(1);
+  auto v3 = v1.erase(2);
+  auto v4 = v2.erase(2);
+  auto v5 = v4.erase(0);
   //            v1 {1, 2, 3, 4}
   //             /           \
   //         v2 {1, 3, 4}     v3 {1, 2, 4}
@@ -93,52 +93,114 @@ TEST(PersistentListTests, EraseList) {
   //    v4 {1, 3}
   //        /
   //    v5 {3}
-  ASSERT_EQ(v2.Find(0), 1);
-  ASSERT_EQ(v2.Find(1), 3);
-  ASSERT_EQ(v3.Find(0), 1);
-  ASSERT_EQ(v3.Find(1), 2);
-  ASSERT_EQ(v3.Find(2), 4);
-  ASSERT_EQ(v4.Find(0), 1);
-  ASSERT_EQ(v4.Find(1), 3);
-  ASSERT_ANY_THROW(v4.Find(2), 4);
-  ASSERT_EQ(v5.Find(0), 3);
+  ASSERT_EQ(v2.find(0), 1);
+  ASSERT_EQ(v2.find(1), 3);
+  ASSERT_EQ(v3.find(0), 1);
+  ASSERT_EQ(v3.find(1), 2);
+  ASSERT_EQ(v3.find(2), 4);
+  ASSERT_EQ(v4.find(0), 1);
+  ASSERT_EQ(v4.find(1), 3);
+  ASSERT_ANY_THROW(v4.find(2), 4);
+  ASSERT_EQ(v5.find(0), 3);
 }
 
-// сделать push_back
+//todo сделать push_back
 TEST(PersistentListTests, InsertList) {
-  PersistentList v1({1, 2, 3, 4});
-  auto v2 = v1.Insert(1, 5);
-  auto v3 = v1.Insert(1, 6);
-  auto v4 = v2.Insert(1, 7);
-  auto v5 = v4.Insert(0, 8);
+  PersistentList<int> v1({1, 2, 3, 4});
+  auto v2 = v1.insert(1, 5);
+  auto v3 = v1.insert(1, 6);
+  auto v4 = v2.insert(1, 7);
+  auto v5 = v4.insert(0, 8);
   //                       v1 {1, 2, 3, 4}
   //                       /           \
   //         v2 {1, 5, 2, 3, 4}     v3 {1, 6, 2, 3, 4}
-  //                     /               \
-  //    v4 {1, 7, 5, 2, 3, 4}       v6 {1, 6, 2, 3, 4, 9}
+  //                     /               
+  //    v4 {1, 7, 5, 2, 3, 4}       
   //                   /
   //    v5 {8, 1, 7, 5, 2, 3, 4}
-  ASSERT_EQ(v2.Find(0), 1);
-  ASSERT_EQ(v2.Find(1), 5);
-  ASSERT_EQ(v2.Find(2), 2);
-  ASSERT_EQ(v2.Find(3), 3);
-  ASSERT_EQ(v2.Find(4), 4);
-  ASSERT_EQ(v3.Find(0), 1);
-  ASSERT_EQ(v3.Find(1), 6);
-  ASSERT_EQ(v3.Find(2), 2);
-  ASSERT_EQ(v3.Find(3), 3);
-  ASSERT_EQ(v3.Find(4), 4);
-  ASSERT_EQ(v4.Find(0), 1);
-  ASSERT_EQ(v4.Find(1), 7);
-  ASSERT_EQ(v4.Find(2), 5);
-  ASSERT_EQ(v4.Find(3), 2);
-  ASSERT_EQ(v4.Find(4), 3);
-  ASSERT_EQ(v4.Find(5), 4);
-  ASSERT_EQ(v5.Find(0), 8);
-  ASSERT_EQ(v5.Find(1), 1);
-  ASSERT_EQ(v5.Find(2), 7);
-  ASSERT_EQ(v5.Find(3), 5);
-  ASSERT_EQ(v5.Find(4), 2);
-  ASSERT_EQ(v5.Find(5), 3);
-  ASSERT_EQ(v5.Find(6), 4);
+  ASSERT_EQ(v2.find(0), 1);
+  ASSERT_EQ(v2.find(1), 5);
+  ASSERT_EQ(v2.find(2), 2);
+  ASSERT_EQ(v2.find(3), 3);
+  ASSERT_EQ(v2.find(4), 4);
+  ASSERT_EQ(v3.find(0), 1);
+  ASSERT_EQ(v3.find(1), 6);
+  ASSERT_EQ(v3.find(2), 2);
+  ASSERT_EQ(v3.find(3), 3);
+  ASSERT_EQ(v3.find(4), 4);
+  ASSERT_EQ(v4.find(0), 1);
+  ASSERT_EQ(v4.find(1), 7);
+  ASSERT_EQ(v4.find(2), 5);
+  ASSERT_EQ(v4.find(3), 2);
+  ASSERT_EQ(v4.find(4), 3);
+  ASSERT_EQ(v4.find(5), 4);
+  ASSERT_EQ(v5.find(0), 8);
+  ASSERT_EQ(v5.find(1), 1);
+  ASSERT_EQ(v5.find(2), 7);
+  ASSERT_EQ(v5.find(3), 5);
+  ASSERT_EQ(v5.find(4), 2);
+  ASSERT_EQ(v5.find(5), 3);
+  ASSERT_EQ(v5.find(6), 4);
+}
+
+TEST(PersistentListTests, UndoList) {
+  PersistentList<int> v1({1, 2, 3, 4});
+  auto v2 = v1.insert(1, 5);
+  auto v1_undo = v2.undo();
+  auto v3 = v1.insert(1, 6);
+  auto v4 = v2.insert(1, 7);
+  auto v4_undo = v4.undo();
+  auto v4_undo_undo = v4_undo.undo();
+  auto v4_undo_undo_redo = v4_undo_undo.redo();
+  //                       v1 {1, 2, 3, 4}
+  //                       /           \
+  //         v2 {1, 5, 2, 3, 4}     v3 {1, 6, 2, 3, 4}
+  //                     /               
+  //    v4 {1, 7, 5, 2, 3, 4}      
+  ASSERT_EQ(v1_undo.find(1), 2);
+  ASSERT_EQ(v4_undo_undo.find(1), 2);
+  ASSERT_EQ(v4_undo_undo_redo.find(1), 5);
+}
+
+TEST(PersistentListTests, IteratorValueTest) {
+  std::shared_ptr<ListOrder> listOrder = std::make_shared<ListOrder>();
+  listOrder->add(1);
+  ListIterator<int> iterator(1, std::make_shared<ListNode<int>>(1, 10, nullptr, nullptr, CmpByListVersion(listOrder)));
+  ASSERT_EQ(*iterator, 10);
+}
+
+TEST(PersistentListTests, IteratorSumList) {
+  PersistentList<int> v1({1, 2, 3, 4});
+  auto v2 = v1.set(0, -1);
+  //                v1 {1, 2, 3, 4}
+  //                   /           
+  //         v2 {-1, 2, 3, 4}    
+  int sum = 0;
+  for (auto i = v2.begin(); i != v2.end(); ++i) {
+    sum += *i;
+  }
+  ASSERT_EQ(sum, 8);
+  sum = 0;
+  for (auto i = v1.begin(); i != v1.end(); ++i) {
+    sum += *i;
+  }
+  ASSERT_EQ(sum, 10);
+}
+
+TEST(PersistentListTests, ReverseIteratorSumList) {
+  PersistentList<int> v1({1, 2, 3, 4});
+  auto v2 = v1.set(0, -1);
+  //                v1 {1, 2, 3, 4}
+  //                   /
+  //         v2 {-1, 2, 3, 4}
+  int sum = 0;
+  for (auto i = v2.rbegin(); i != v2.rend(); ++i) {
+    sum += *i;
+  }
+  ASSERT_EQ(sum, 8);
+  sum = 0;
+  for (auto i = v1.rbegin(); i != v1.rend(); ++i) {
+    sum += *i;
+  }
+  ASSERT_EQ(sum, 10);
 }
